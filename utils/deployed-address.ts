835568bef877efefb2e08966f11c2d33f8aa18c3
putri martin tanjung <putrimartintanjoeng@gmail.com>
2024-08-16 13:45:39 +0000

    update

--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,4 +1,4 @@
-export const SWTRProxy = '0xea65D3f80d2253293a64e2f8DdF4CEC401823053'
-export const ProxyAdmin = '0x6a0baE5CF96d14e39FA9F5AdB4fadABb55B22E17'
-export const SWTRImplementation = '0xE4e93a474734E22A4f660EF1fE7aC3fc096E856c'
+export const SWTRProxy = '0xB0B35Ed6eBb115d8a7Aa71D693a09c9051b3d763'
+export const ProxyAdmin = '0xe1a347dBE2de08D8eaF8a073b8Ca9fC3A6C60Aad'
+export const SWTRImplementation = '0xdE2F53B26C78f53c45d196100f0ff8dc96f435A9'
     
